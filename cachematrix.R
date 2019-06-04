4ec1e3a4018f6296a420690b28ce20c4588f043a
WellPreservedPineapple <50838673+WellPreservedPineapple@users.noreply.github.com>
2019-06-04 10:45:53 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## These functions work to calculate and cache the inverse of a matrix you supply.
+## The next function will skip the inverse calculations and return
+## the inverse if you supply a matrix which has its inverse already cached.
+## If you supply a new matrix it will proceed to calculate the inverse then 
+## return it.
 
 
-## Write a short comment describing this function
+## This function is very similar to the example function given to us.
+## I just made a few small changes to set it for the solve function rather
+## than mean.
+ makeCacheMatrix <- function(x = matrix()) {
+         inv <- NULL
+         set <- function(y) {
+                 x <<- y
+                 inv <<- NULL
+             }
+         get <- function() x
+         setsolve <- function(solveMatrix) inv <<- solveMatrix
+         getsolve <- function() inv
+         list(set = set, get = get, 
+                         setsolve = setsolve,
+                         getsolve = getsolve)
+     
+         }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+     
+ ## Also very similar to the example; this function is only different
+ ## in that I set it to pull/solve the inverse rather than the mean.
+     
+     cacheSolve <- function(x, ...) {
+             inv <- x$getsolve()
+             if(!is.null(inv)) {
+                     message("I gotchu with that inverse.")
+                     return(inv)
+                 }
+             data <- x$get()
+             inv <- solve(data)
+             x$setsolve(inv)
+             inv
+         }
